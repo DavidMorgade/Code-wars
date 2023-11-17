402e26cde04f387f3a1f17131fbd15c5eeb202de
Morgade <92484871+DavidMorgade@users.noreply.github.com>
2023-11-17 09:51:18 +0100

    Create LoveFriendship.cs

---
+++ C#/7 Kyu/LoveFriendship.cs
@@ -0,0 +1,29 @@
+/*
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+*/
+
+using System;
+
+public static class Kata
+{
+  public static int WordsToMarks(string str)
+    {
+        int value = 0;
+        foreach (char c in str)
+        {
+            value += c - 'a' + 1;
+        }
+
+        return value;
+    }
+}
