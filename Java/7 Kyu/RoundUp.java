703534ee57e6f2a7b8a3ff6234df79ef9c0b3bd6
Morgade <92484871+DavidMorgade@users.noreply.github.com>
2024-02-16 11:30:45 +0100

    Java Kyu 7

---
+++ Java/7 Kyu/RoundUp.java
@@ -0,0 +1,34 @@
+/*
+
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+*/
+
+package com.codewars.hwtdstrngls;
+
+public class RoundToTheNextMultipleOf5 {
+
+  public static int roundToNext5(int number) {
+    boolean finish = number % 5 == 0;
+    while(!finish) {
+      if(number % 5 == 0) return number;
+      number++;
+    }
+    return number;
+  }
+}
