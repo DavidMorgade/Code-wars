af94873863646cc395b01003398f7bcbf995248a
Morgade <92484871+DavidMorgade@users.noreply.github.com>
2024-03-14 11:42:14 +0100

    Java Kyu 8

---
+++ Java/8 Kyu/StringCombiner.java
@@ -0,0 +1,18 @@
+/*
+Combine strings function
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+*/
+
+
+public class StringCombiner {
+    
+    public static String combineNames(String first,String last) {
+
+        return first + " " + last;
+    }
+}
